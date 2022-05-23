fb644914397b5f6bd21d331f9d88ad70957cdfa7
salmanca <mohammedsalmanfaris.c.a@gmail.com>
2022-05-23 12:37:15 +0530

    database migration command

--- prokart/settings.py
+++ prokart/settings.py
@@ -132,12 +132,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'usersite/static'),
     os.path.join(BASE_DIR, 'adminsite/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 MEDIA_URL = 'uploaded/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded')
 
